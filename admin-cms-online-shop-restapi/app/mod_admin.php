63f318e143da7a5a07732b8200544c3d3eecf96b
yudapw37 <junkiesdota24@gmail.com>
2021-03-02 07:32:37 +0700

    add admin cms rest api

---
+++ admin-cms-online-shop-restapi/app/mod_admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mod_admin extends Model
+{
+    protected $table = 'ms_admin';
+    protected $fillable = ['code_perusahaan', 'username', 'password','keyhash', 'email', 'nama', 'no_telp', 'code_jabatan', 'kodeAdminTrx', 'created_at', 'updated_at' ];
+}
